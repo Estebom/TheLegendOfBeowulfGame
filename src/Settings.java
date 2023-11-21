02a9ab94204f99896a628cf77004a46113bda226
Estebom <estebomofficial@gmail.com>
2023-11-20 23:18:39 -0500

    added settings

--- src/Settings.java
+++ src/Settings.java
@@ -15,7 +15,7 @@ public class Settings extends JPanel {
         this.setLayout(new GridBagLayout());
 
 
-        GridBagConstraints gbcFeatures = new GridBagConstraints();
+
 
         goBack = new JButton("Go Back to Main Menu");
         goBack.setPreferredSize(new Dimension(50, 50));
@@ -23,30 +23,11 @@ public class Settings extends JPanel {
         goBack.addActionListener(buttonListener);
 
 
-        //name.setPreferredSize(new Dimension(100, 100));
-        //name.setVisible(true);
-        gbcFeatures.gridx = 0;
-        gbcFeatures.gridy = 0;
-        gbcFeatures.fill = GridBagConstraints.HORIZONTAL;
-        gbcFeatures.weightx = 1.0;
-        gbcFeatures.insets = new Insets(20, 20, 20, 20);
 
 
-        gbcFeatures.gridy++;
 
 
-        gbcFeatures.gridy++;
 
-
-        gbcFeatures.gridy++;
-
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.NONE;
 
         this.add(goBack);
         this.setVisible(true);
