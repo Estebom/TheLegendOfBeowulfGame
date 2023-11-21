b4981b92eb30821def4b675224dd40e139ef61d0
Estebom <estebomofficial@gmail.com>
2023-11-20 22:36:09 -0500

    updates resolution to 1080p

--- src/MainDisplay.java
+++ src/MainDisplay.java
@@ -9,25 +9,52 @@ import java.awt.*;
 /*
 Class for the main display of the game
 @author Esteban Rodriguez
- */
+*/
+
+//public class MainDisplay extends JFrame {
+//    private MainMenu mainMenu;
+//    private NewGame newGame;
+//
+//    private GamePlay gamePlay;
+//    public MainDisplay(){
+//
+//        mainMenu = new MainMenu(this);
+//        newGame = new NewGame(this);
+//        gamePlay = new GamePlay(this);
+//
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.setResizable(false);
+//        //this.setUndecorated(true);  //will be used during real implementation
+//
+//
+//        this.setSize(new Dimension(2560, 1440));
+//        this.setLayout((new CardLayout()));
+//
+//        this.add(mainMenu, "MainMenu");
+//        this.add(newGame, "NewGame");
+//        this.add(gamePlay, "START");
+//
+//        showMainMenu();
+//        this.setVisible(true);
+//
+//    }
+
 public class MainDisplay extends JFrame {
     private MainMenu mainMenu;
     private NewGame newGame;
-
     private GamePlay gamePlay;
-    public MainDisplay(){
 
+    public MainDisplay() {
         mainMenu = new MainMenu(this);
         newGame = new NewGame(this);
         gamePlay = new GamePlay(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        //this.setUndecorated(true);  //will be used during real implementation
 
-
-        this.setSize(new Dimension(2560, 1440));
-        this.setLayout((new CardLayout()));
+        // Set size for 1080p resolution
+        this.setSize(new Dimension(1920, 1080));
+        this.setLayout(new CardLayout());
 
         this.add(mainMenu, "MainMenu");
         this.add(newGame, "NewGame");
@@ -35,8 +62,9 @@ public class MainDisplay extends JFrame {
 
         showMainMenu();
         this.setVisible(true);
-
     }
+
+
     public void showMainMenu(){
         ((CardLayout)this.getContentPane().getLayout()).show(this.getContentPane(), "MainMenu");
     }
