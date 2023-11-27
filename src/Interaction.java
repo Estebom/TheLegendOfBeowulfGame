c76ee94d73f57ec5c2d204d287d5eb11916f4ef9
alfonsolavila <124621079+alfonsolavila@users.noreply.github.com>
2023-11-26 23:19:03 -0500

    Update Interaction.java

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,3 +1,24 @@
-public class Interaction {
-    public Interaction(){}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ItemInteraction implements ActionListener {
+    private Interactions interactions;
+
+    public ItemInteraction(Interactions interactions) {
+        this.interactions = interactions;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Grabbing an item
+        grabItem(new Item("Sample Item", "Sample Item Description"));
+    }
+
+    public void grabItem(Item item) {
+        interactions.grabItem(item);
+    }
+
+    public void grabWeapon(Weapon weapon) {
+        interactions.grabWeapon(weapon);
+    }
 }
