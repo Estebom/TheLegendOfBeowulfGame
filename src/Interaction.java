d83abe0230db75f8ac85ebc39e6627a510eeb439
alfonsolavila <124621079+alfonsolavila@users.noreply.github.com>
2023-12-02 00:00:34 -0500

    Interaction.java

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,19 +1,33 @@
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.awt.event.ActionListener;
 
 public class Interaction implements ActionListener {
     private Inventory inventory;
 
     public Interaction() {
-
         this.inventory = Inventory.getInstance();
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Grabbing an item
-        grabItem(new Item("Sample Item", "Sample Item Description"));
+        // Sample logic for grabbing an item or weapon from a chest
+        Chest chest = new Chest("Sample Chest");
+        Item item = chest.giveItem();
+        Weapon weapon = chest.giveWeapon();
+
+        // Add the grabbed item and weapon to the player's inventory
+        grabItem(item);
+        grabWeapon(weapon);
+
+        // Optionally, you can perform additional actions based on the specific item or weapon
+        if (weapon instanceof LongSword) {
+            ((LongSword) weapon).pushBack();
+        } else if (weapon instanceof MidSword) {
+            ((MidSword) weapon).holdOff();
+        } else if (weapon instanceof ShortSword) {
+            ((ShortSword) weapon).criticalHits();
+        }
     }
 
     public void grabItem(Item item) {
