2b4bb1b3f1664c7531acfb6c2a3f643d9f03d669
Iacopo Nohea Lenzi <102275256+Nohealenzi@users.noreply.github.com>
2023-12-04 21:18:15 -0500

    Update GameState.java
    Removed netbeans package

--- src/GameState.java
+++ src/GameState.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.legendofbeowulf;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
